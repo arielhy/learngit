e261586fe07f7af08fca3cbe09b9668c10ade192
xyaohua <yao.huang@tieto.com>
2016-06-30 13:41:15 +0800

    add the second line

--- test_vp.sh
+++ test_vp.sh
@@ -7,3 +7,4 @@ vp_folder="/production/maps/map_$series/official/$version/variant_pkg"
 unzip -l ${vp_folder}/$2.zip | grep ".cdt" | awk 'BEGIN { FS="/" } { print $NF }' | cut -d"." -f1 > $2_qb.txt
 
 #Add this line for test
+#Second line for test
